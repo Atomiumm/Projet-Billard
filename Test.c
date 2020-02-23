3d4eac9eeb96d1335d54c6622af7aaf903e67a96
Atomiumm <51803097+Atomiumm@users.noreply.github.com>
2020-02-23 21:23:08 +0100

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+struct test {
+	int x;
+	int y;
+};
+
+struct test temp = {1, 2};
+
 int main(){
-	printf("test");
+	temp = {3, 4};
+	printf("%d, %d", temp.x, temp.y);
 
     return 0;
 }
