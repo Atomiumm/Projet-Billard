d6e9fdbcb26098e775acaa689f1f568dd316c2ed
Atomiumm <51803097+Atomiumm@users.noreply.github.com>
2020-02-26 10:13:25 +0100

    Update Pix2Pos3.c

--- Pix2Pos3.c
+++ Pix2Pos3.c
@@ -18,55 +18,61 @@ struct coordinate {
 
 
 /* Global Variables */
-	struct coordinate Red = {-1, -1, -1};
-	struct coordinate Yellow = {-1, -1, -1};
-	struct coordinate White = {-1, -1, -1};
-	
-	short signed int BallDiameter = 11;
-	struct coordinate TableMin = {15, 15, 0};
-	struct coordinate TableMax = {85, 65, 0};
-	struct colour RBallMin = {160, 0, 0};
-	struct colour RBallMax = {255, 160, 160};
-	struct colour YBallMin = {140, 140, 0};
-	struct colour YBallMax = {255, 255, 175};
-	struct colour WBallMin = {100, 100, 100};
-	struct colour WBallMax = {255, 255, 255};
-	struct colour BGMin = {39, 91, 202};
-	struct colour BGMax = {62, 116, 255};
-	//struct colour LimMin = {0, 0, 121};
-	//struct colour LimMax = {14, 42, 126};
-	struct colour LimMin = {0, 0, 121};
-	struct colour LimMax = {0, 54, 180};
-
-	struct coordinate TileAmount;
-	struct coordinate *Tiles;
-
-	int PixelWidth = 0, PixelHeight = 0;
-	FILE *PixmapBin;
-	FILE *PosTxt;
-
-	unsigned int *PixelInt;
-
-	short int ErrorFlag = 0;
+	/*Results*/
+		struct coordinate Red = {-1, -1, -1};
+		struct coordinate Yellow = {-1, -1, -1};
+		struct coordinate White = {-1, -1, -1};
+	/*Program arguments*/
+		short signed int BallDiameter = 11;
+		struct coordinate TableMin = {15, 15, 0};
+		struct coordinate TableMax = {85, 65, 0};
+		struct colour RBallMin = {160, 0, 0};
+		struct colour RBallMax = {255, 160, 160};
+		struct colour YBallMin = {140, 140, 0};
+		struct colour YBallMax = {255, 255, 175};
+		struct colour WBallMin = {100, 100, 100};
+		struct colour WBallMax = {255, 255, 255};
+		struct colour BGMin = {39, 91, 202};
+		struct colour BGMax = {62, 116, 255};
+		//struct colour LimMin = {0, 0, 121};
+		//struct colour LimMax = {14, 42, 126};
+		struct colour LimMin = {0, 0, 121};
+		struct colour LimMax = {0, 54, 180};
+	/*Tiles*/
+		struct coordinate TileAmount;
+		struct coordinate *Tiles;
+	/*Files and size*/
+		int PixelWidth = 0, PixelHeight = 0;
+		FILE *PixmapBin;
+		FILE *PosTxt;
+	/*Pixelmap*/
+		unsigned int *PixelInt;
+		//
+	/*Errorflag*/
+		short int ErrorFlag = 0;
+		//
 
 
 /* Function definition */
+	/*Converting a decimal colour to a RGB colour*/
 struct colour Int2Colour(int ColourInt) {
 	struct colour ColourRGB;
-	if(ColourInt < 0 || ColourInt > 16777215){
-		ColourRGB.R = -1;
-		ColourRGB.G = -1;		//Ca me fait chier et on doit changer
-		ColourRGB.B = -1;
-		return ColourRGB;
-	}
-	ColourRGB.B = ColourInt % 256;
-	ColourInt /= 256;
-	ColourRGB.G = ColourInt % 256;
-	ColourInt /= 256;
-	ColourRGB.R = ColourInt;
+	/*Detecting if int not in correct range*/
+		if(ColourInt < 0 || ColourInt > 16777215){
+			ColourRGB.R = -1;
+			ColourRGB.G = -1;
+			ColourRGB.B = -1;
+			return ColourRGB;
+		}
+	/*Calculating colour*/
+		ColourRGB.B = ColourInt % 256;
+		ColourInt /= 256;
+		ColourRGB.G = ColourInt % 256;
+		ColourInt /= 256;
+		ColourRGB.R = ColourInt;
 	return ColourRGB;
 }
-
+	/*Calculating the amount of pixels that correspond to the given range*/
 int GetScore(struct coordinate Coordinates, int DeltaX, int DeltaY, struct colour RangeMin, struct colour RangeMax){
 	int Score = 0;
 	for(int x = Coordinates.X; x < Coordinates.X + DeltaX; x++){
@@ -78,7 +84,7 @@ int GetScore(struct coordinate Coordinates, int DeltaX, int DeltaY, struct colou
 	}
 	return Score;
 }
-
+	/*Tiles the map with tiles of size Balldiameter*/
 void MapTile(){
 	for(int TileY = 0; TileY - TileAmount.Y; TileY++){
 		for(int TileX = 0; TileX - TileAmount.X; TileX++){
@@ -92,157 +98,174 @@ void MapTile(){
 		}
 	}
 }
-
+	/*Makes the tiles converge to the maximum score*/
 void Converge(struct colour RangeMin, struct colour RangeMax){
-	for(int index = 0; index < TileAmount.X * TileAmount.Y; index++) Tiles[index].Score = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
-	int flag = 1;
-	while(flag){
-		flag = 0;
+	/*Calculating first score*/
+		for(int index = 0; index < TileAmount.X * TileAmount.Y; index++) Tiles[index].Score = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
+		//
+	/*Iterating through the tiles and move those who have a score to a place with bigger score*/
+		int flag = 1;
+		while(flag){
+			flag = 0;
+			for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
+				if(Tiles[index].Score){
+					/*Evaluating the possible movements*/
+						Tiles[index].Y++;
+						int up = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
+						Tiles[index].Y -= 2;
+						int down = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
+						Tiles[index].Y++;
+						Tiles[index].X++;
+						int right = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
+						Tiles[index].X -= 2;
+						int left = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
+						Tiles[index].X++;
+					/*Move to the best spot*/
+						if(up > Tiles[index].Score){
+							Tiles[index].Y++;
+							Tiles[index].Score = up;
+							flag = 1;
+						}
+						if(down > Tiles[index].Score){
+							Tiles[index].Y--;
+							Tiles[index].Score = down;
+							flag = 1;
+						}
+						if(right > Tiles[index].Score){
+							Tiles[index].X++;
+							Tiles[index].Score = right;
+							flag = 1;
+						}
+						if(left > Tiles[index].Score){
+							Tiles[index].X--;
+							Tiles[index].Score = left;
+							flag = 1;
+						}
+				}
+			}
+		}
+}
+	/*Find the balls*/
+void CheckForBalls(){
+	/*Find the red balls*/
+		MapTile();
+		Converge(RBallMin, RBallMax);
+	/*Check if there is only one and save it*/
 		for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
 			if(Tiles[index].Score){
-				Tiles[index].Y++;
-				int up = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
-				Tiles[index].Y -= 2;
-				int down = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
-				Tiles[index].Y++;
-				Tiles[index].X++;
-				int right = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
-				Tiles[index].X -= 2;
-				int left = GetScore(Tiles[index], BallDiameter, BallDiameter, RangeMin, RangeMax);
-				Tiles[index].X++;
-				if(up > Tiles[index].Score){
-					Tiles[index].Y++;
-					Tiles[index].Score = up;
-					flag = 1;
+				if(Red.X < 0){
+					Red.X = Tiles[index].X;
+					Red.Y = Tiles[index].Y;
+					Red.Score = Tiles[index].Score;
 				}
-				if(down > Tiles[index].Score){
-					Tiles[index].Y--;
-					Tiles[index].Score = down;
-					flag = 1;
-				}
-				if(right > Tiles[index].Score){
-					Tiles[index].X++;
-					Tiles[index].Score = right;
-					flag = 1;
-				}
-				if(left > Tiles[index].Score){
-					Tiles[index].X--;
-					Tiles[index].Score = left;
-					flag = 1;
+				else{
+					if(Red.X != Tiles[index].X || Red.Y != Tiles[index].Y){
+						printf("Error : multiple red balls, continuing with x=%d and y=%d\n", Red.X, Red.Y);
+						exit(0);
+					}
 				}
 			}
 		}
-	}
+	/*Find the yellow balls*/
+		MapTile();
+		Converge(YBallMin, YBallMax);
+	/*Check if there is only one and save it*/
+		for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
+			if(Tiles[index].Score){
+				if(Yellow.X < 0){
+					Yellow.X = Tiles[index].X;
+					Yellow.Y = Tiles[index].Y;
+					Yellow.Score = Tiles[index].Score;
+				}
+				else{
+					if(Yellow.X != Tiles[index].X || Yellow.Y != Tiles[index].Y){
+						printf("Error : multiple yellow balls, cannot continue\n");
+						exit(0);
+					}
+				}
+			}
+		}
+	/*Find the white balls*/
+		MapTile();
+		Converge(WBallMin, WBallMax);
+	/*Check if there is only one and save it*/
+		for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
+			if(Tiles[index].Score){
+				if(White.X < 0){
+					White.X = Tiles[index].X;
+					White.Y = Tiles[index].Y;
+					White.Score = Tiles[index].Score;
+				}
+				else{
+					if(White.X != Tiles[index].X || White.Y != Tiles[index].Y){
+						printf("Error : multiple white balls, cannot continue\n");
+						exit(0);
+					}
+				}
+			}
+		}
+	/*Check if the balls are overlapping*/
+		if((Red.X + 11 > Yellow.X && Red.X - 11 < Yellow.X) && (Red.Y + 11 > Yellow.Y && Red.Y - 11 < Yellow.Y)){
+			printf("Error : red ball and yellow ball overlapping, cannot continue\n");
+			ErrorFlag = 1;
+		}
+		if((Red.X + 11 > White.X && Red.X - 11 < White.X) && (Red.Y + 11 > White.Y && Red.Y - 11 < White.Y)){
+			printf("Error : red ball and white ball overlapping, cannot continue\n");
+			ErrorFlag = 1;
+		}
+		if((White.X + 11 > Yellow.X && White.X - 11 < Yellow.X) && (White.Y + 11 > Yellow.Y && White.Y - 11 < Yellow.Y)){
+			printf("Error : white ball and yellow ball overlapping, cannot continue\n");
+			ErrorFlag = 1;
+		}
+		if(ErrorFlag) exit(0);
 }
-
-void CheckForBalls(){
-	MapTile();
-	Converge(RBallMin, RBallMax);
-	for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
-		if(Tiles[index].Score){
-			if(Red.X < 0){
-				Red.X = Tiles[index].X;
-				Red.Y = Tiles[index].Y;
-				Red.Score = Tiles[index].Score;
-			}
-			else{
-				if(Red.X != Tiles[index].X || Red.Y != Tiles[index].Y){
-					printf("Error : multiple red balls, continuing with x=%d and y=%d\n", Red.X, Red.Y);
-					exit(0);
-				}
-			}
-		}
-	}
-	MapTile();
-	Converge(YBallMin, YBallMax);
-	for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
-		if(Tiles[index].Score){
-			if(Yellow.X < 0){
-				Yellow.X = Tiles[index].X;
-				Yellow.Y = Tiles[index].Y;
-				Yellow.Score = Tiles[index].Score;
-			}
-			else{
-				if(Yellow.X != Tiles[index].X || Yellow.Y != Tiles[index].Y){
-					printf("Error : multiple yellow balls, cannot continue\n");
-					exit(0);
-				}
-			}
-		}
-	}
-	MapTile();
-	Converge(WBallMin, WBallMax);
-	for(int index = 0; index < TileAmount.X * TileAmount.Y; index++){
-		if(Tiles[index].Score){
-			if(White.X < 0){
-				White.X = Tiles[index].X;
-				White.Y = Tiles[index].Y;
-				White.Score = Tiles[index].Score;
-			}
-			else{
-				if(White.X != Tiles[index].X || White.Y != Tiles[index].Y){
-					printf("Error : multiple white balls, cannot continue\n");
-					exit(0);
-				}
-			}
-		}
-	}
-	if((Red.X + 11 > Yellow.X && Red.X - 11 < Yellow.X) && (Red.Y + 11 > Yellow.Y && Red.Y - 11 < Yellow.Y)){
-		printf("Error : red ball and yellow ball overlapping, cannot continue\n");
-		ErrorFlag = 1;
-	}
-	if((Red.X + 11 > White.X && Red.X - 11 < White.X) && (Red.Y + 11 > White.Y && Red.Y - 11 < White.Y)){
-		printf("Error : red ball and white ball overlapping, cannot continue\n");
-		ErrorFlag = 1;
-	}
-	if((White.X + 11 > Yellow.X && White.X - 11 < Yellow.X) && (White.Y + 11 > Yellow.Y && White.Y - 11 < Yellow.Y)){
-		printf("Error : white ball and yellow ball overlapping, cannot continue\n");
-		ErrorFlag = 1;
-	}
-	if(ErrorFlag) exit(0);
-}
-
+	/*Find the table*/
 void FindTable(){
-	ErrorFlag = 1;
-	for(int y = 0; y < PixelHeight; y++){
-		struct coordinate Coordinates = {PixelWidth/2, y, 0};
-		if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
-			TableMin.Y = y;
-			ErrorFlag = 0;
-			break;
+	/*Find the top*/
+		ErrorFlag = 1;
+		for(int y = 0; y < PixelHeight; y++){
+			struct coordinate Coordinates = {PixelWidth/2, y, 0};
+			if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
+				TableMin.Y = y;
+				ErrorFlag = 0;
+				break;
+			}
 		}
-	}
-	ErrorFlag = 1;
-	for(int x = 0; x < PixelWidth; x++){
-		struct coordinate Coordinates = {x, PixelHeight/2, 0};
-		if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
-			TableMin.X = x;
-			ErrorFlag = 0;
-			break;
+	/*Find the left*/
+		ErrorFlag = 1;
+		for(int x = 0; x < PixelWidth; x++){
+			struct coordinate Coordinates = {x, PixelHeight/2, 0};
+			if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
+				TableMin.X = x;
+				ErrorFlag = 0;
+				break;
+			}
 		}
-	}
-	ErrorFlag = 1;
-	for(int y = PixelHeight; y > 0; y--){
-		struct coordinate Coordinates = {PixelWidth/2, y, 0};
-		if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
-			TableMax.Y = y+1;
-			ErrorFlag = 0;
-			break;
+	/*Find the bottom*/
+		ErrorFlag = 1;
+		for(int y = PixelHeight; y > 0; y--){
+			struct coordinate Coordinates = {PixelWidth/2, y, 0};
+			if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
+				TableMax.Y = y+1;
+				ErrorFlag = 0;
+				break;
+			}
 		}
-	}
-	ErrorFlag = 1;
-	for(int x = PixelWidth; x > 0; x--){
-		struct coordinate Coordinates = {x, PixelHeight/2, 0};
-		if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
-			TableMax.X = x+1;
-			ErrorFlag = 0;
-			break;
+	/*Find the right*/
+		ErrorFlag = 1;
+		for(int x = PixelWidth; x > 0; x--){
+			struct coordinate Coordinates = {x, PixelHeight/2, 0};
+			if(GetScore(Coordinates, 1, 1, LimMin, LimMax)){
+				TableMax.X = x+1;
+				ErrorFlag = 0;
+				break;
+			}
 		}
-	}
-	if(ErrorFlag){
-		printf("Error : Table not found\n");
-		exit(0);
-	}
+	/*Check if everything went fine*/
+		if(ErrorFlag){
+			printf("Error : Table not found\n");
+			exit(0);
+		}
 }
 
 
