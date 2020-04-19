166b60c2d30b93119de1d9b64ecf6c6988aa5d9b
Cocococococococococococococo <corentin.rochet@gmail.com>
2020-04-19 18:08:26 +0200

    Functional matlab code

--- matlab_script_dev.m
+++ matlab_script_dev.m
@@ -1,5 +1,3 @@
-%% initialisation
-
 clear;clc;
 
 pos = %s;
@@ -9,21 +7,14 @@ y_min = %d;
 y_max = %d;
 ball_diam = %d;
 
-%pos : n*6 matrix containing (xred, yred, xyel, yyel, xwhite, ywhite) for
-%each n valid frame
-
-%% plot of the trajectories
-
 plot(pos(:,1),pos(:,2),'r-',pos(:,3),pos(:,4),'y-',pos(:,5),pos(:,6),'k-');
 axis ij
-
-%% detection of the first moving ball to detect player ball, and creation of arrays of interest
 
 posleft = [pos;pos(end,:);pos(end,:)];
 posright = [pos(1,:);pos(1,:);pos];
 pos = [pos(1,:);pos;pos(end,:)];
 
-n_frames = numel(pos)/6; %not real frames but osef
+n_frames = numel(pos)/6;
 
 vel = (posright-posleft)/2;
 acc = (posright-2*pos+posleft)/4;
@@ -52,15 +43,6 @@ else
     ball_b = 2;
 end
 
-%% logical arrays to identify the frames where player ball touches the bands and other balls
-
-% a band is touched if a player ball acceleration is detected in the right
-% direction while the ball is close enough to the corresponding band
-
-% a ball is touched if both balls accelerates while being close enough
-
-%a ajouter : tests de proximité dépendants de la vitesse
-
 band_touch = (velleft(:,2*player_ball-1) < 0 & vel(:,2*player_ball-1) > 0 & (pos(:,2*player_ball-1)-x_min) < 2*ball_diam) | (velleft(:,2*player_ball-1) > 0 & vel(:,2*player_ball-1) < 0 & (-pos(:,2*player_ball-1)+x_max) < 3*ball_diam) | (velleft(:,2*player_ball) < 0 & vel(:,2*player_ball) > 0 & (pos(:,2*player_ball)-y_min) < 2*ball_diam) | (velleft(:,2*player_ball) > 0 & vel(:,2*player_ball) < 0 & (-pos(:,2*player_ball)+y_max) < 3*ball_diam);
 ball_touch_a = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_a-1)) > 0 | abs(acc(:,2*ball_a)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_a-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_a)).^2) < 12*ball_diam^2;
 ball_touch_b = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_b-1)) > 0 | abs(acc(:,2*ball_b)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_b-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_b)).^2) < 12*ball_diam^2;
@@ -68,8 +50,6 @@ ball_touch_b = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0)
 band_touch=find(band_touch);
 ball_touch_a = find(ball_touch_a);
 ball_touch_b = find(ball_touch_b);
-
-%% checking for winning condition : both balls are touched and 3 bands inbetween
 
 win = 0;
 
