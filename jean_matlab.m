4e2584791d6013f74a9920d95092a39b5c10b021
Cocococococococococococococo <corentin.rochet@gmail.com>
2020-05-15 00:24:44 +0200

    matlab modifications

--- jean_matlab.m
+++ jean_matlab.m
@@ -1,6 +1,6 @@
 %% initialisation
 
-clear;clc;
+clear;clc; close all
 
 pos = %s;
 ball_diam = %d;
@@ -8,15 +8,16 @@ ball_diam = %d;
 %pos : n*6 matrix containing (xred, yred, xyel, yyel, xwhite, ywhite) for
 %each n valid frame
 
-%%
+%% detection of billard boundaries
 
-x_min = min(pos([1 3 5],:));
-x_max = max(pos([1 3 5],:));
-y_min = min(pos([2 4 6],:));
-y_max = max(pos([2 4 6],:));
+x_min = min(min(pos(:,[1 3 5])));
+x_max = max(max(pos(:,[1 3 5])));
+y_min = min(min(pos(:,[2 4 6])));
+y_max = max(max(pos(:,[2 4 6])));
+
 %% plot of the trajectories
 
-figure(1)
+ScoreSheet = figure(1);
 
 hold on
 
@@ -47,7 +48,7 @@ moving(:,2) = (pos(:,3)-pos(1,3)).^2 + (pos(:,4)-pos(1,4)).^2 >= 100;
 moving(:,3) = (pos(:,5)-pos(1,5)).^2 + (pos(:,6)-pos(1,6)).^2 >= 100;
 
 moving_ind = find(moving);
-player_ball = ceil(min(moving_ind(mod(moving_ind,n_frames)==min(mod(moving_ind,n_frames))))/n_frames);
+player_ball = ceil(min(moving_ind(mod(moving_ind-1,n_frames)==min(mod(moving_ind-1,n_frames))))/n_frames);
 fprintf("player ball : \%d\\n",player_ball);
 
 if player_ball == 1
@@ -64,33 +65,51 @@ end
 
 %% logical arrays to identify the frames where player ball touches the bands and other balls
 
+% creating comparative velocity to avoid both false positive and false
+% negative during proximity test
+
+vel_comp = sqrt(max(vel(:,2*player_ball-1).^2+vel(:,2*player_ball).^2,max(velleft(:,2*player_ball-1).^2+velleft(:,2*player_ball).^2,velright(:,2*player_ball-1).^2+velright(:,2*player_ball).^2)));
+
+vel_comp_a_sq = ball_diam^2 + max((vel(:,2*player_ball-1)-vel(:,2*ball_a-1)).^2+(vel(:,2*player_ball)-vel(:,2*ball_a)).^2,max((velleft(:,2*player_ball-1)-velleft(:,2*ball_a-1)).^2+(velleft(:,2*player_ball)-velleft(:,2*ball_a)).^2,(velright(:,2*player_ball-1)-velright(:,2*ball_a-1)).^2+(velright(:,2*player_ball)-velright(:,2*ball_a)).^2));
+vel_comp_b_sq = ball_diam^2 + max((vel(:,2*player_ball-1)-vel(:,2*ball_b-1)).^2+(vel(:,2*player_ball)-vel(:,2*ball_b)).^2,max((velleft(:,2*player_ball-1)-velleft(:,2*ball_b-1)).^2+(velleft(:,2*player_ball)-velleft(:,2*ball_b)).^2,(velright(:,2*player_ball-1)-velright(:,2*ball_b-1)).^2+(velright(:,2*player_ball)-velright(:,2*ball_b)).^2));
+
 % a band is touched if a player ball acceleration is detected in the right
 % direction while the ball is close enough to the corresponding band
 
 % a ball is touched if both balls accelerates while being close enough
 
-%a ajouter : tests de proximité dépendants de la vitesse
-
-band_touch_vert = (velleft(:,2*player_ball-1) < 0 & velright(:,2*player_ball-1) > 0 & (pos(:,2*player_ball-1)-x_min) <= 2*ball_diam) | (velleft(:,2*player_ball-1) > 0 & velright(:,2*player_ball-1) < 0 & (-pos(:,2*player_ball-1)+x_max) < 3*ball_diam);
-band_touch_hrzt = (velleft(:,2*player_ball) < 0 & velright(:,2*player_ball) > 0 & (pos(:,2*player_ball)-y_min) < 2*ball_diam) | (velleft(:,2*player_ball) > 0 & velright(:,2*player_ball) < 0 & (-pos(:,2*player_ball)+y_max) < 3*ball_diam);
-ball_touch_a = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_a-1)) > 0 | abs(acc(:,2*ball_a)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_a-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_a)).^2) < 12*ball_diam^2;
-ball_touch_b = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_b-1)) > 0 | abs(acc(:,2*ball_b)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_b-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_b)).^2) < 12*ball_diam^2;
+band_touch_left = velleft(:,2*player_ball-1) < 0 & velright(:,2*player_ball-1) > 0 & (pos(:,2*player_ball-1)-x_min) <= vel_comp;
+band_touch_right = velleft(:,2*player_ball-1) > 0 & velright(:,2*player_ball-1) < 0 & (-pos(:,2*player_ball-1)+x_max) <= vel_comp;
+band_touch_up = velleft(:,2*player_ball) < 0 & velright(:,2*player_ball) > 0 & (pos(:,2*player_ball)-y_min) <= vel_comp;
+band_touch_down = velleft(:,2*player_ball) > 0 & velright(:,2*player_ball) < 0 & (-pos(:,2*player_ball)+y_max) <= vel_comp;
+ball_touch_a = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_a-1)) > 0 | abs(acc(:,2*ball_a)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_a-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_a)).^2) < vel_comp_a_sq;
+ball_touch_b = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_b-1)) > 0 | abs(acc(:,2*ball_b)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_b-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_b)).^2) < vel_comp_b_sq;
 
 %% delete double count
 
-band_touch_vert_left = [band_touch_vert(2:end);band_touch_vert(end)];
-band_touch_vert_hrzt_left = [band_touch_hrzt(2:end);band_touch_hrzt(end)];
+band_touch_left_left = [band_touch_left(2:end);band_touch_left(end)];
+band_touch_right_left = [band_touch_right(2:end);band_touch_right(end)];
+band_touch_up_left = [band_touch_up(2:end);band_touch_up(end)];
+band_touch_down_left = [band_touch_down(2:end);band_touch_down(end)];
 ball_touch_a_left = [ball_touch_a(2:end);ball_touch_a(end)];
 ball_touch_b_left = [ball_touch_b(2:end);ball_touch_b(end)];
 
-band_touch_vert(band_touch_vert & band_touch_vert_left) = 0;
-band_touch_hrzt(band_touch_hrzt & band_touch_hrzt_left) = 0;
+band_touch_left(band_touch_left & band_touch_left_left) = 0;
+band_touch_right(band_touch_right & band_touch_right_left) = 0;
+band_touch_up(band_touch_up & band_touch_up_left) = 0;
+band_touch_down(band_touch_down & band_touch_down_left) = 0;
 ball_touch_a(ball_touch_a & ball_touch_a_left) = 0;
 ball_touch_b(ball_touch_b & ball_touch_b_left) = 0;
 
-band_touch=find(band_touch_vert | band_touch_hrzt);
+band_touch=find(band_touch_left | band_touch_right | band_touch_up | band_touch_down);
 ball_touch_a = find(ball_touch_a);
 ball_touch_b = find(ball_touch_b);
+
+%% showing band and ball touch on graph
+
+plot(pos(band_touch,2*player_ball-1),pos(band_touch,2*player_ball),'*');
+plot(pos(ball_touch_a,2*player_ball-1),pos(ball_touch_a,2*player_ball),'o');
+plot(pos(ball_touch_b,2*player_ball-1),pos(ball_touch_b,2*player_ball),'o');
 
 %% checking for winning condition : both balls are touched and 3 bands inbetween
 
@@ -114,5 +133,7 @@ else
 end
 
 fprintf("win : \%d\\n",win);
+
+saveas(ScoreSheet,'ScoreSheet','pdf');
 
 return
--- matlab_script_dev.m
+++ matlab_script_dev.m
@@ -1,14 +1,24 @@
-clear;clc;
+clear;clc; close all
 
-pos = %s;
-x_min = %d;
-x_max = %d;
-y_min = %d;
-y_max = %d;
-ball_diam = %d;
+pos = [[681,180,160,187,213,334];[681,180,188,256,213,334];[682,180,231,324,210,355];[681,180,287,334,195,342];[681,180,341,346,184,275];[681,180,390,357,173,210];[682,180,436,369,162,151];[681,180,481,382,153,100];[681,180,529,389,150,137];[682,180,576,381,149,180];[681,180,625,375,147,218];[681,180,677,369,145,253];[681,180,724,364,142,285];[681,180,704,346,140,316];[681,180,664,326,137,348];[681,180,629,307,135,379];[681,180,598,289,134,374];[681,180,569,271,134,352];[681,180,542,253,134,330];[681,180,517,235,135,313];[681,180,493,219,135,295];[681,180,468,201,135,278];[681,180,444,184,135,261];[681,180,420,167,135,243];[681,180,396,151,135,226];[681,180,372,133,135,209];[681,180,348,117,135,193];[681,180,325,101,135,176];[681,180,302,108,135,160];[681,180,278,118,135,143];[681,180,255,127,136,127];[681,180,232,137,135,111];[682,180,209,146,135,100];[681,180,186,155,136,112];[681,180,163,164,136,121];[681,180,140,173,136,130];[681,180,138,184,136,139];[681,180,155,198,136,148];[681,180,169,210,136,157];[681,180,181,223,135,166];[681,180,194,235,136,175];[681,180,206,248,135,183];[681,180,219,260,135,192];[681,180,231,272,135,200];[681,180,243,284,135,208];[681,180,255,296,135,217];[681,180,267,308,135,225];[681,180,280,320,135,233];[681,180,291,332,135,241];[681,180,302,344,135,249];[681,180,314,356,135,257];[681,180,326,367,134,264];[681,180,338,379,134,272];[681,180,349,390,134,279];[681,180,360,383,134,287];[681,180,372,376,133,294];[681,180,382,370,133,301];[681,180,394,364,133,308];[681,180,404,357,133,316];[681,180,414,352,133,323];[681,180,426,345,133,329];[682,180,436,339,133,336];[681,180,446,333,133,343];[682,180,456,326,132,349];[681,180,466,321,132,356];[681,180,476,315,132,362];[681,180,486,309,132,368];[681,180,496,303,132,374];[681,180,506,297,132,380];[681,180,516,291,131,386];[681,180,526,286,131,390];[681,180,536,280,131,387];[681,180,545,274,131,383];[681,180,554,269,132,380];[681,180,564,263,131,377];[681,180,572,258,132,374];[681,180,582,253,132,371];[681,180,592,248,132,369];[681,180,600,242,132,366];[681,180,610,236,132,363];[681,180,618,231,132,360];[681,180,628,226,132,358];[681,180,636,221,132,356];[681,180,644,216,132,353];[681,180,653,211,132,351];[681,180,662,206,132,349];[681,180,670,201,132,347];[681,180,678,196,132,345];[681,179,686,192,132,343];[681,177,696,190,132,341];[681,176,704,187,132,339];[681,173,712,185,132,337];[681,172,720,182,132,336];[681,169,728,180,132,334];[681,168,731,177,131,333];[681,166,726,174,132,331];[681,165,722,171,131,330];[681,163,718,169,131,329];[681,162,714,166,131,328];[681,161,709,163,131,327];[680,160,705,160,130,326];[680,159,700,158,130,325];[680,158,696,155,130,324];[680,157,693,153,130,324];[679,156,691,150,130,323];[676,156,690,147,130,323];[675,155,689,145,130,322];[673,155,688,142,131,322];[671,154,686,139,131,321];[670,154,685,137,130,321];[668,154,684,134,130,321];[667,154,683,132,130,321];[665,154,682,130,130,321];[665,153,681,128,131,321];[663,153,680,126,131,321];[663,153,679,124,131,321];[662,153,678,122,131,321];[661,153,677,120,131,321];[660,153,676,118,131,321];[659,153,675,117,131,321];[659,153,674,115,131,321];[658,153,674,114,131,321];[657,153,673,112,131,321];[657,153,672,111,131,321];[657,153,672,110,131,321];[657,153,670,109,131,321]];
+ball_diam = 11;
+
+x_min = min(min(pos(:,[1 3 5])));
+x_max = max(max(pos(:,[1 3 5])));
+y_min = min(min(pos(:,[2 4 6])));
+y_max = max(max(pos(:,[2 4 6])));
+
+figure(1)
+
+hold on
 
 plot(pos(:,1),pos(:,2),'r-',pos(:,3),pos(:,4),'y-',pos(:,5),pos(:,6),'k-');
 axis ij
+
+line([x_min x_max],[y_min y_min]);
+line([x_min x_max],[y_max y_max]);
+line([x_min x_min],[y_min y_max]);
+line([x_max x_max],[y_min y_max]);
 
 posleft = [pos;pos(end,:);pos(end,:)];
 posright = [pos(1,:);pos(1,:);pos];
@@ -22,14 +32,13 @@ acc = (posright-2*pos+posleft)/4;
 velleft=[vel(2:end,:);vel(end,:)];
 velright=[vel(1,:);vel(1:end-1,:)];
 
-vel_bl = abs(vel)>1;
-moving(:,1) = vel_bl(:,1) | vel_bl(:,2);
-moving(:,2) = vel_bl(:,3) | vel_bl(:,4);
-moving(:,3) = vel_bl(:,5) | vel_bl(:,6);
+moving(:,1) = (pos(:,1)-pos(1,1)).^2 + (pos(:,2)-pos(1,2)).^2 >= 100;
+moving(:,2) = (pos(:,3)-pos(1,3)).^2 + (pos(:,4)-pos(1,4)).^2 >= 100;
+moving(:,3) = (pos(:,5)-pos(1,5)).^2 + (pos(:,6)-pos(1,6)).^2 >= 100;
 
 moving_ind = find(moving);
-player_ball = min(mod(moving_ind,n_frames));
-fprintf("player ball : \%d\\n",player_ball);
+player_ball = ceil(min(moving_ind(mod(moving_ind-1,n_frames)==min(mod(moving_ind-1,n_frames))))/n_frames);
+fprintf("player ball : %d\n",player_ball);
 
 if player_ball == 1
     ball_a = 2;
@@ -43,33 +52,59 @@ else
     ball_b = 2;
 end
 
-band_touch = (velleft(:,2*player_ball-1) < 0 & vel(:,2*player_ball-1) > 0 & (pos(:,2*player_ball-1)-x_min) < 2*ball_diam) | (velleft(:,2*player_ball-1) > 0 & vel(:,2*player_ball-1) < 0 & (-pos(:,2*player_ball-1)+x_max) < 3*ball_diam) | (velleft(:,2*player_ball) < 0 & vel(:,2*player_ball) > 0 & (pos(:,2*player_ball)-y_min) < 2*ball_diam) | (velleft(:,2*player_ball) > 0 & vel(:,2*player_ball) < 0 & (-pos(:,2*player_ball)+y_max) < 3*ball_diam);
-ball_touch_a = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_a-1)) > 0 | abs(acc(:,2*ball_a)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_a-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_a)).^2) < 12*ball_diam^2;
-ball_touch_b = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_b-1)) > 0 | abs(acc(:,2*ball_b)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_b-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_b)).^2) < 12*ball_diam^2;
+vel_comp = sqrt(max(vel(:,2*player_ball-1).^2+vel(:,2*player_ball).^2,max(velleft(:,2*player_ball-1).^2+velleft(:,2*player_ball).^2,velright(:,2*player_ball-1).^2+velright(:,2*player_ball).^2)));
 
-band_touch=find(band_touch);
-ball_touch_a = find(ball_touch_a);
-ball_touch_b = find(ball_touch_b);
+vel_comp_a_sq = ball_diam^2 + max((vel(:,2*player_ball-1)-vel(:,2*ball_a-1)).^2+(vel(:,2*player_ball)-vel(:,2*ball_a)).^2,max((velleft(:,2*player_ball-1)-velleft(:,2*ball_a-1)).^2+(velleft(:,2*player_ball)-velleft(:,2*ball_a)).^2,(velright(:,2*player_ball-1)-velright(:,2*ball_a-1)).^2+(velright(:,2*player_ball)-velright(:,2*ball_a)).^2));
+vel_comp_b_sq = ball_diam^2 + max((vel(:,2*player_ball-1)-vel(:,2*ball_b-1)).^2+(vel(:,2*player_ball)-vel(:,2*ball_b)).^2,max((velleft(:,2*player_ball-1)-velleft(:,2*ball_b-1)).^2+(velleft(:,2*player_ball)-velleft(:,2*ball_b)).^2,(velright(:,2*player_ball-1)-velright(:,2*ball_b-1)).^2+(velright(:,2*player_ball)-velright(:,2*ball_b)).^2));
+
+band_touch_left = velleft(:,2*player_ball-1) < 0 & velright(:,2*player_ball-1) > 0 & (pos(:,2*player_ball-1)-x_min) <= vel_comp;
+band_touch_right = velleft(:,2*player_ball-1) > 0 & velright(:,2*player_ball-1) < 0 & (-pos(:,2*player_ball-1)+x_max) <= vel_comp;
+band_touch_up = velleft(:,2*player_ball) < 0 & velright(:,2*player_ball) > 0 & (pos(:,2*player_ball)-y_min) <= vel_comp;
+band_touch_down = velleft(:,2*player_ball) > 0 & velright(:,2*player_ball) < 0 & (-pos(:,2*player_ball)+y_max) <= vel_comp;
+ball_touch_a = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_a-1)) > 0 | abs(acc(:,2*ball_a)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_a-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_a)).^2) < vel_comp_a_sq;
+ball_touch_b = (abs(acc(:,2*player_ball-1)) > 0 | abs(acc(:,2*player_ball)) > 0) & (abs(acc(:,2*ball_b-1)) > 0 | abs(acc(:,2*ball_b)) > 0) & ((pos(:,2*player_ball-1)-pos(:,2*ball_b-1)).^2 + (pos(:,2*player_ball)-pos(:,2*ball_b)).^2) < vel_comp_b_sq;
+
+band_touch_left_left = [band_touch_left(2:end);band_touch_left(end)];
+band_touch_right_left = [band_touch_right(2:end);band_touch_right(end)];
+band_touch_up_left = [band_touch_up(2:end);band_touch_up(end)];
+band_touch_down_left = [band_touch_down(2:end);band_touch_down(end)];
+ball_touch_a_left = [ball_touch_a(2:end);ball_touch_a(end)];
+ball_touch_b_left = [ball_touch_b(2:end);ball_touch_b(end)];
+
+band_touch_left(band_touch_left & band_touch_left_left) = 0;
+band_touch_right(band_touch_right & band_touch_right_left) = 0;
+band_touch_up(band_touch_up & band_touch_up_left) = 0;
+band_touch_down(band_touch_down & band_touch_down_left) = 0;
+ball_touch_a(ball_touch_a & ball_touch_a_left) = 0;
+ball_touch_b(ball_touch_b & ball_touch_b_left) = 0;
+
+band_touch_ind=find(band_touch_left | band_touch_right | band_touch_up | band_touch_down);
+ball_touch_a_ind = find(ball_touch_a);
+ball_touch_b_ind = find(ball_touch_b);
+
+plot(pos(band_touch_ind,2*player_ball-1),pos(band_touch_ind,2*player_ball),'*');
+plot(pos(ball_touch_a_ind,2*player_ball-1),pos(ball_touch_a_ind,2*player_ball),'o');
+plot(pos(ball_touch_b_ind,2*player_ball-1),pos(ball_touch_b_ind,2*player_ball),'o');
 
 win = 0;
 
-if numel(ball_touch_a)==0 || numel(ball_touch_b)==0 || numel(band_touch)<3
-    fprintf("win : \%d\\n",win);
+if numel(ball_touch_a_ind)==0 || numel(ball_touch_b_ind)==0 || numel(band_touch_ind)<3
+    fprintf("win : %d\n",win);
     return
 end
 
-if max(ball_touch_a) < min(ball_touch_b)
-    band_touch = band_touch(band_touch>max(ball_touch_a) & band_touch<min(ball_touch_b));
-    if numel(band_touch)>=3
+if max(ball_touch_a_ind) < min(ball_touch_b_ind)
+    band_touch_ind = band_touch_ind(band_touch_ind>max(ball_touch_a_ind) & band_touch_ind<min(ball_touch_b_ind));
+    if numel(band_touch_ind)>=3
         win = 1;
     end
 else
-    band_touch = band_touch(band_touch>max(ball_touch_b) & band_touch<min(ball_touch_a));
-    if numel(band_touch)>=3
+    band_touch_ind = band_touch_ind(band_touch_ind>max(ball_touch_b_ind) & band_touch_ind<min(ball_touch_a_ind));
+    if numel(band_touch_ind)>=3
         win = 1;
     end
 end
 
-fprintf("win : \%d\\n",win);
+fprintf("win : %d\n",win);
 
 return
